8ed5ca0b9e208b586ab6fa8c3611b41706601ee1
Arnab Manna <arnabmanna203@gmail.com>
2023-08-15 18:35:34 +0530

    make authentication and ready the dashboard page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,9 @@
-import {Button} from '@/components/ui/button';
+
+import { UserButton } from '@clerk/nextjs';
 export default function DashboardPage() {
   return (
-    <p>Dashboard Page</p>
+    <div>
+      <p>Dashboard Page (protected)</p>
+    </div>
   )
 }
